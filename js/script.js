4656ef117ecfc17a30e0da8c7f1675de8bfb436f
mjaumi <mjaumi2864@gmail.com>
2023-02-19 19:39:48 +0600

    dynamic match adding with state done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // selecting DOM elements
-const singleScoreEl = document.querySelectorAll('.lws-singleResult');
-const incrementFormEl = document.querySelectorAll('.incrementForm');
-const decrementFormEl = document.querySelectorAll('.decrementForm');
+let singleScoreEl = document.querySelectorAll('.lws-singleResult');
+let incrementFormEl = document.querySelectorAll('.incrementForm');
+let decrementFormEl = document.querySelectorAll('.decrementForm');
 
 const matchContainerEl = document.getElementById('matchContainer');
 const addAnotherMatchBtn = document.getElementById('addNewMatch');
@@ -9,6 +9,9 @@ const addAnotherMatchBtn = document.getElementById('addNewMatch');
 // action identifiers
 const INCREMENT = 'increment';
 const DECREMENT = 'decrement';
+const INSERT = 'insert';
+
+let matchCounter;
 
 // action creators
 const increment = (value) => {
@@ -31,43 +34,58 @@ const decrement = (value) => {
     };
 }
 
+const insert = () => {
+    return {
+        type: INSERT,
+    };
+}
+
 // initial state
 const initialState = [
     {
         id: 1,
-        score: 0
-    },
-    {
-        id: 2,
         score: 0
     }
 ];
 
 // create score reducer function
 function scoreReducer(state = initialState, action) {
-    const updatedScore = state.map((s) => {
-        if (s.id === action?.payload?.id) {
-            if (action.type === INCREMENT) {
-                return {
-                    ...s,
-                    score: s.score + action.payload.value
-                };
-            } else if (action.type === DECREMENT) {
-                return {
-                    ...s,
-                    score: s.score < action.payload.value ? 0 : s.score - action.payload.value
-                };
+    let updatedScore = [];
+
+    if (action.type === INSERT) {
+        updatedScore = [
+            ...state,
+            {
+                id: state[state.length - 1].id + 1,
+                value: 0
+            }
+        ];
+    } else {
+        updatedScore = state.map((s) => {
+            if (s.id === action?.payload?.id) {
+                if (action.type === INCREMENT) {
+                    return {
+                        ...s,
+                        score: s.score + action.payload.value
+                    };
+                } else if (action.type === DECREMENT) {
+                    return {
+                        ...s,
+                        score: s.score < action.payload.value ? 0 : s.score - action.payload.value
+                    };
+                } else {
+                    return {
+                        ...s
+                    };
+                }
             } else {
                 return {
                     ...s
                 };
             }
-        } else {
-            return {
-                ...s
-            };
-        }
-    });
+        });
+    }
+
     return updatedScore;
 }
 
@@ -78,6 +96,8 @@ const store = Redux.createStore(scoreReducer);
 const render = () => {
     const state = store.getState();
     singleScoreEl[0].innerHTML = state[0].score;
+    matchCounter = state.length + 1;
+    console.log(state.length);
 }
 
 // update UI initially
@@ -104,27 +124,33 @@ addAnotherMatchBtn.addEventListener('click', () => {
     const newMatchDiv = document.createElement('div');
     newMatchDiv.classList.add('match');
     newMatchDiv.innerHTML = `
-        <div class="wrapper">
-            <button class="lws-delete">
-              <img src="./image/delete.svg" alt="" />
-            </button>
-            <h3 class="lws-matchName">Match 1</h3>
-          </div>
-          <div class="inc-dec">
-            <form class="incrementForm">
-              <h4>Increment</h4>
-              <input type="number" name="increment" class="lws-increment" />
-            </form>
-            <form class="decrementForm">
-              <h4>Decrement</h4>
-              <input type="number" name="decrement" class="lws-decrement" />
-            </form>
-          </div>
-          <div class="numbers">
-            <h2 class="lws-singleResult">120</h2>
-          </div>
-        </div>
+    <div class="wrapper">
+    <button class="lws-delete">
+    <img src="./image/delete.svg" alt="" />
+    </button>
+    <h3 class="lws-matchName">Match ${matchCounter}</h3>
+    </div>
+    <div class="inc-dec">
+    <form class="incrementForm">
+    <h4>Increment</h4>
+    <input type="number" name="increment" class="lws-increment" />
+    </form>
+    <form class="decrementForm">
+    <h4>Decrement</h4>
+    <input type="number" name="decrement" class="lws-decrement" />
+    </form>
+    </div>
+    <div class="numbers">
+    <h2 class="lws-singleResult">120</h2>
+    </div>
+    </div>
     `;
-
     matchContainerEl.appendChild(newMatchDiv);
+
+    // inserting new children into query selector
+    singleScoreEl = document.querySelectorAll('.lws-singleResult');
+    incrementFormEl = document.querySelectorAll('.incrementForm');
+    decrementFormEl = document.querySelectorAll('.decrementForm');
+
+    store.dispatch(insert());
 });
